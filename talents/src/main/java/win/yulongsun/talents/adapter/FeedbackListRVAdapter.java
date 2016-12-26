ec668530dac8c278afa5d97c78fca35559bc26b0
yulongsun007 <a87533887>
2016-12-26 17:26:55 +0800

    1.修改ui

--- talents/src/main/java/win/yulongsun/talents/adapter/FeedbackListRVAdapter.java
+++ talents/src/main/java/win/yulongsun/talents/adapter/FeedbackListRVAdapter.java
@@ -29,7 +29,7 @@ public class FeedbackListRVAdapter extends SuperAdapter<UserPlanR> {
         holder.setText(R.id.tv_feedback_job_name, item.job.tmp_job_name);
         holder.setText(R.id.tv_feedback_company_name, item.company.company_name);
         holder.setText(R.id.tv_feedback_update_at, item.update_at);
-        holder.setText(R.id.tv_feedback_resume_id, "简历# " + item.resume_id);
+        holder.setText(R.id.tv_feedback_resume_id, "[简历]# " + item.resume_id);
 
         String status = null;
         switch (item.apply_status) {
@@ -39,7 +39,7 @@ public class FeedbackListRVAdapter extends SuperAdapter<UserPlanR> {
             case Constant.APPLY_STATUS.REFERRER_REJECT:
                 status = "推荐人已拒绝";
                 holder.setVisibility(R.id.tv_feedback_apply_msg, View.VISIBLE);
-                holder.setText(R.id.tv_feedback_apply_msg, item.apply_msg);
+                holder.setText(R.id.tv_feedback_apply_msg, "理由：" + item.apply_msg);
                 break;
             case Constant.APPLY_STATUS.REFERRER_PASS:
                 status = "推荐人已推荐";
@@ -47,12 +47,12 @@ public class FeedbackListRVAdapter extends SuperAdapter<UserPlanR> {
             case Constant.APPLY_STATUS.HR_REJECT:
                 status = "HR已拒绝";
                 holder.setVisibility(R.id.tv_feedback_apply_msg, View.VISIBLE);
-                holder.setText(R.id.tv_feedback_apply_msg, item.apply_msg);
+                holder.setText(R.id.tv_feedback_apply_msg, "理由：" + item.apply_msg);
                 break;
             case Constant.APPLY_STATUS.HR_PASS:
                 status = "HR已通过";
                 holder.setVisibility(R.id.tv_feedback_apply_msg, View.VISIBLE);
-                holder.setText(R.id.tv_feedback_apply_msg, item.apply_msg);
+                holder.setText(R.id.tv_feedback_apply_msg, "理由：" + item.apply_msg);
                 break;
         }
         holder.setText(R.id.tv_feedback_apply_status, status);
--- talents/src/main/java/win/yulongsun/talents/ui/hr/talent/TalentListFragment.java
+++ talents/src/main/java/win/yulongsun/talents/ui/hr/talent/TalentListFragment.java
@@ -1,14 +1,31 @@
 package win.yulongsun.talents.ui.hr.talent;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.EditText;
+
+import com.zhy.http.okhttp.OkHttpUtils;
+import com.zhy.http.okhttp.callback.StringCallback;
 
 import org.greenrobot.eventbus.EventBus;
 
+import okhttp3.Call;
 import win.yulongsun.framework.adapter.SuperAdapter;
+import win.yulongsun.framework.util.JsonUtil;
+import win.yulongsun.framework.util.android.widget.ToastUtils;
+import win.yulongsun.framework.util.java.lang.StringUtils;
 import win.yulongsun.talents.R;
-import win.yulongsun.talents.adapter.ReferrerListRVAdapter;
+import win.yulongsun.talents.adapter.ReferrerResumeListRVAdapter;
 import win.yulongsun.talents.base.CommonListFragment;
+import win.yulongsun.talents.common.Constant;
+import win.yulongsun.talents.entity.Resume;
+import win.yulongsun.talents.entity.UserPlanR;
 import win.yulongsun.talents.event.StartBrotherEvent;
+import win.yulongsun.talents.http.resp.ResponseList;
+import win.yulongsun.talents.http.resp.biz.UserPlanRResponse;
+import win.yulongsun.talents.ui.stu.resume.ResumeEditFragment;
 
 /**
  * Created by yulongsun
@@ -27,7 +44,7 @@ public class TalentListFragment extends CommonListFragment {
 
     @Override
     protected SuperAdapter getAdapter() {
-        return new ReferrerListRVAdapter(_mActivity, _mDatas, R.layout.item_fragment_list);
+        return new ReferrerResumeListRVAdapter(_mActivity, _mDatas, R.layout.item_resume_list);
     }
 
     @Override
@@ -39,11 +56,86 @@ public class TalentListFragment extends CommonListFragment {
     protected void initData() {
         super.initData();
         _mToolbar.setVisibility(View.GONE);
+        loadDataFromServer("user_plan_r/hrListStu", _mUser, UserPlanRResponse.class);
     }
 
     @Override
     public void onItemClick(View itemView, int viewType, int position) {
         super.onItemClick(itemView, viewType, position);
-        EventBus.getDefault().post(new StartBrotherEvent(TalentDetailFragment.newInstance(position)));
+        UserPlanR userPlanR = (UserPlanR) _mDatas.get(0);
+        Resume resume = userPlanR.resume;
+        EventBus.getDefault().post(new StartBrotherEvent(ResumeEditFragment.newInstance(Constant.MODE_VALUE.CHECK, resume)));
+    }
+
+    @Override
+    public void onItemLongClick(View itemView, int viewType, int position) {
+        super.onItemLongClick(itemView, viewType, position);
+        final UserPlanR userPlanR = (UserPlanR) _mDatas.get(position);
+        final View dialogView = LayoutInflater.from(_mActivity).inflate(R.layout.dialog_referrer_edit, null);
+        final EditText dialogMsg = (EditText) dialogView.findViewById(R.id.et_dialog_msg);
+        new AlertDialog.Builder(_mActivity)
+                .setTitle("推荐")
+                .setView(dialogView)
+                .setPositiveButton("同意", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(final DialogInterface dialog, int which) {
+                        String apply_msg = dialogMsg.getText().toString();
+                        if (StringUtils.isEmpty(apply_msg)) {
+                            ToastUtils.toastL(_mActivity, "请输入操作理由");
+                            return;
+                        }
+
+                        OkHttpUtils.post()
+                                .url(Constant.URL + "user_plan_r/referrerDoResume")
+                                .addParams("_id", String.valueOf(userPlanR._id))
+                                .addParams("apply_status", String.valueOf(Constant.APPLY_STATUS.HR_PASS))
+                                .addParams("apply_msg", apply_msg)
+                                .build().execute(new StringCallback() {
+                            @Override
+                            public void onError(Call call, Exception e, int id) {
+
+                            }
+
+                            @Override
+                            public void onResponse(String response, int id) {
+                                ResponseList resp = (ResponseList) JsonUtil.fromJson(response, ResponseList.class);
+                                ToastUtils.toastL(_mActivity, resp.msg);
+                                dialog.dismiss();
+                                initData();
+                            }
+                        });
+                    }
+                })
+                .setNeutralButton("拒绝", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(final DialogInterface dialog, int which) {
+                        String apply_msg = dialogMsg.getText().toString();
+                        if (StringUtils.isEmpty(apply_msg)) {
+                            ToastUtils.toastL(_mActivity, "请输入操作理由");
+                            return;
+                        }
+
+                        OkHttpUtils.post()
+                                .url(Constant.URL + "user_plan_r/referrerDoResume")
+                                .addParams("_id", String.valueOf(userPlanR._id))
+                                .addParams("apply_status", String.valueOf(Constant.APPLY_STATUS.HR_REJECT))
+                                .addParams("apply_msg", apply_msg)
+                                .build().execute(new StringCallback() {
+                            @Override
+                            public void onError(Call call, Exception e, int id) {
+
+                            }
+
+                            @Override
+                            public void onResponse(String response, int id) {
+                                ResponseList resp = (ResponseList) JsonUtil.fromJson(response, ResponseList.class);
+                                ToastUtils.toastL(_mActivity, resp.msg);
+                                dialog.dismiss();
+                                initData();
+                            }
+                        });
+                    }
+                }).setNegativeButton("取消", null)
+                .show();
     }
 }
